b2431426facd9567448b3f4950cf5e7578ca7212
acasada <abcasada@gmail.com>
2025-01-30 10:55:16 -0600

    update run.bat to auto-pull updates from git

--- run.bat
+++ run.bat
@@ -1,1 +1,12 @@
+cd "C:\ProgramFiles\Kasa-Time-Entry"
+git remote update
+if not "%errorlevel%"=="0" exit /b %errorlevel%
+setlocal EnableDelayedExpansion
+set commitMessage=
+for /f %%i in ('git rev-parse --abbrev-ref HEAD') do (set /p commitMessage=<nul & git log --oneline ..origin/%%i)
+if not "!commitMessage!"=="" (
+    git pull origin %%i
+)
+endlocal
+
 python "C:\ProgramFiles\Kasa-Time-Entry\src\main.py"
